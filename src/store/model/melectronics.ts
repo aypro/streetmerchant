2567b02acbe0a56a94cd9db555bc3c82e7d05922
aypro <aypro2k@gmail.com>
2021-02-06 09:20:59 +0100

    removed cards from melectronics

--- src/store/model/melectronics.ts
+++ src/store/model/melectronics.ts
@@ -40,37 +40,13 @@ export const Melectronics: Store = {
 			model: 'strix',
 			series: '3080',
             url: 'https://www.melectronics.ch/fr/p/785300155941/asus-rog-strix-rtx3080-o10g-gaming'
-        },       
-		{
-			brand: 'gainward',
-			model: 'phoenix gs',
-			series: '3080',
-            url: 'https://www.melectronics.ch/fr/p/785300155935/gainward-geforce-rtx3080-phoenix-gs-10gb'
-		},    
+        },         
 		{
 			brand: 'asus',
 			model: 'tuf',
 			series: '3080',
             url: 'https://www.melectronics.ch/fr/p/785300155939/asus-tuf-geforce-rtx-3080-10g-gaming'
         },  		    
-		{
-			brand: 'gainward',
-			model: 'phoenix',
-			series: '3080',
-            url: 'https://www.melectronics.ch/fr/p/785300155933/gainward-geforce-rtx3080-phoenix-10gb'
-        },      
-		{
-			brand: 'palit',
-			model: 'gaming pro oc',
-			series: '3080',
-            url: 'https://www.melectronics.ch/fr/p/785300155934/palit-geforce-rtx3080-gaming-pro-oc-10gb'
-        },   
-		{
-			brand: 'palit',
-			model: 'gaming pro',
-			series: '3080',
-            url: 'https://www.melectronics.ch/fr/p/785300155932/palit-geforce-rtx3080-gaming-pro-10gb'
-        },  
 		{
 			brand: 'asus',
 			model: 'strix',
