78c2530e8b5e437bcb55ff1668328cb299b8c406
aypro <aypro2k@gmail.com>
2021-02-12 00:59:20 +0100

    added a bunch of 3070 cards to Digitec

--- src/store/model/digitec.ts
+++ src/store/model/digitec.ts
@@ -15,6 +15,157 @@ export const Digitec: Store = {
 		]
 	},
 	links: [
+		//3070
+		{
+			brand: 'asus',
+			model: 'dual',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13798669'
+        },
+		{
+			brand: 'asus',
+			model: 'dual oc',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13689612'
+        }, 			
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13870737'
+        }, 			
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13894685'
+        }, 			
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13894686'
+        }, 			
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13799104'
+        }, 			
+		{
+			brand: 'msi',
+			model: 'ventus 3x',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13799193'
+        }, 			
+		{
+			brand: 'msi',
+			model: 'ventus 2x',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13799260'
+        }, 					
+		{
+			brand: 'palit',
+			model: 'gaming pro',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/14036311'
+        }, 			
+		{
+			brand: 'zotac',
+			model: 'twin edge',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13752196'
+        }, 			
+		{
+			brand: 'zotac',
+			model: 'twin edge oc',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13913888'
+        }, 			
+		{
+			brand: 'kfa2',
+			model: 'sg oc',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/14079527'
+        }, 			
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13870827'
+        }, 			
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13881950'
+        }, 		
+		{
+			brand: 'evga',
+			model: 'xc3 ultra',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/14036617'
+        }, 			
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/14036624'
+        }, 			
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/14036650'
+        }, 			
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13798659'
+        }, 			
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13798661'
+        }, 			
+		{
+			brand: 'gainward',
+			model: 'phoenix gs',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/14079610'
+        }, 			
+		{
+			brand: 'gainward',
+			model: 'phoenix',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/14079611'
+        }, 			
+		{
+			brand: 'gigabyte',
+			model: 'vision oc',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/14100150'
+        }, 			
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/14100200'
+        }, 			
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13894876'
+        }, 																																																	 			
+		{
+			brand: 'msi',
+			model: 'gaming trio',
+			series: '3070',
+            url: 'https://www.digitec.ch/en/s1/product/13799116'
+        }, 	
 		//5600x
 		{
 			brand: 'amd',
@@ -35,13 +186,7 @@ export const Digitec: Store = {
 			series: 'sonyps5c',
             url: 'https://www.digitec.ch/en/s1/product/12664145'
         },
-		//3070
-		{
-			brand: 'palit',
-			model: 'gaming pro',
-			series: '3070',
-            url: 'https://www.digitec.ch/en/s1/product/14036311'
-        }, 		
+	
 		//3080				         
 		{
 			brand: 'gigabyte',
