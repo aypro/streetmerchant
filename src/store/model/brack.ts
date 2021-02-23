941d7b9d0126b0db2b154ff0d6e719d8275bc735
aypro <aypro2k@gmail.com>
2021-02-24 00:51:02 +0100

    update brack in stock condition

--- src/store/model/brack.ts
+++ src/store/model/brack.ts
@@ -3,6 +3,11 @@ import {Store} from './store';
 export const Brack: Store = {
 	currency: 'CHF',
 	labels: {
+		inStock:
+			{
+	            container: '.productStage__addToCartSubmit',
+            	text: ['Dans le panier']
+        	},		
 		outOfStock:	[
 			{
 	            container: '.productStage__soldOut',
@@ -17,6 +22,7 @@ export const Brack: Store = {
 			model: 'gaming pro',
 			series: '3070',
             url: 'https://www.brack.ch/fr/palit-carte-graphique-geforce-rtx3070-gaming-pro-8-gb-1124733'
+			//url: 'https://www.brack.ch/fr/pny-carte-graphique-nvidia-quadro-p2200-939548'
         }
 	],
 	name: 'brack'
