5d7eceb576aadd0d2bce21a179ac0d4c36f5ddbf
aypro <aypro2k@gmail.com>
2021-02-23 17:17:42 +0100

    added Brack

---
+++ src/store/model/brack.ts
@@ -0,0 +1,23 @@
+import {Store} from './store';
+
+export const Brack: Store = {
+	currency: 'CHF',
+	labels: {
+		outOfStock:	[
+			{
+	            container: '.productStage__soldOut',
+            	text: ['Article épuisé']
+        	}		
+		]
+	},
+	links: [
+		//3070
+		{
+			brand: 'palit',
+			model: 'gaming pro',
+			series: '3070',
+            url: 'https://www.brack.ch/fr/palit-carte-graphique-geforce-rtx3070-gaming-pro-8-gb-1124733'
+        }
+	],
+	name: 'brack'
+};
